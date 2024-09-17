d0c80f628408640fa8e4a5e440e9f62ea17afd35
Heriberto Santana <heriberto.santana@dci-student.org>
2024-09-17 09:15:11 +0200

    user

--- controllers/userControllers/loginUser.js
+++ controllers/userControllers/loginUser.js
@@ -1,11 +1,16 @@
 const User = require("../../models/User");
 const jwt = require("jsonwebtoken");
 
-const generateToken = (id) => {
-  // Generate JWT token
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
-  });
+const generateToken = (user) => {
+  return jwt.sign(
+    {
+      id: user._id,         
+      role: user.role,      
+      email: user.email     
+    },
+    process.env.JWT_SECRET, 
+    { expiresIn: '1h' }     
+  );
 };
 
 const loginUser = async (req, res, next) => {
@@ -40,6 +45,7 @@ const loginUser = async (req, res, next) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      role: user.role,
       token: generateToken(user._id),
     });
   } catch (err) {
