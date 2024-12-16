00879702986fa032f1cd4ddae96c84e2a1563794
Heriberto Santana <heriberto.santana@dci-student.org>
2024-12-16 13:52:03 +0100

    Options

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -16,7 +16,7 @@ const sendEmail = async (options) => {
       from: `"Havenova" <${process.env.EMAIL_FROM}>`,
       to: options.email,
       subject: options.subject,
-      html,
+      html: options.html,
     };
 
     await transporter.sendMail(mailOptions);
